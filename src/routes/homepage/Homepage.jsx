16da9c380340a8ad4fb1b364fd03ee9aef67b6cc
Samir Gyawali <gyawali.samir000@gmail.com>
2024-10-19 10:33:33 +0545

    Dashboard 01

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -5,11 +5,30 @@ import Toogle from '../../components/Toogle';
 const Homepage = () => {
     return (
         <div className='homepage'>
-            <div className="titleWrapper">
-                <h1>Samir Gyawali</h1>
-                <Toogle />
+            <div className="profileWrapper">
+                <div className="titleWrapper">
+                    <h1>Samir Gyawali</h1>
+                    <div className="toogleDiv">
+                        <Toogle />
+                        <span>Mindful Reminder</span>
+                    </div>
+                </div>
+                <img src="../../assets/user.png" alt="" />
             </div>
-            <img src="../../assets/user.png" alt="" />
+            <div className="stats">
+                <div className="totalEntries">
+                    <span className='title'>Total Entries</span>
+                    <span className='total block'>35</span>
+                </div>
+                <div className="attentionSeeking">
+                    <span className='title'>Attention Seeking</span>
+                    <span className='attention block'>10</span>
+                </div>
+                <div className="pleasureDriven">
+                    <span className='title'>Pleasure Driven</span>
+                    <span className='pleasure block'>15</span>
+                </div>
+            </div>
         </div>
     );
 };
